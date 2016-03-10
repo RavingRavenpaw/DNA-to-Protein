63b299dd22a7d7369d7994996749ead71775ac7f
Raven <chromefusion44@gmail.com>
2016-03-09 20:45:19 -0500

    Initialization!
    (copy/pasted this from the program, realized it would be a better idea
    to put it here then to have my giant mess/wall of text in the program)
    
    This program is going to convert between DNA, RNA, and amino acid
    sequences!
    
    #PSUEDOCODE YEAH
    #DNA -> RNA
    #RNA -> Amino Acids
    #Amino Acids -> Protein (vague folding simulation)
    #
    #Okay so basically
    '''
    Converting to and from DNA and RNA will be pretty easy.
    Only trouble I forsee is implementing a system that
    allows you to choose with strand of the DNA you wish
    to use, and then convert that to a single strand of RNA.
    
    It doesn't matter because you don't lose any information,
    but biology is complicated and I don't know if the strand
    chosen matters or is important to some processes.
    
    Going from RNA to an Amino Acid chain will be pretty easy
    as well, but will require some additional research on my
    part because I don't know what base pairs make what amino
    acids. It can't be that hard though. There's only like
    20 of them or so, right?
    
    As for proteins, I have no fucking clue how they fold.
    I know it's complicated and looks pretty scary
    (helices and fold and whatnot!!), but maybe I could
    detect patterns in the amino acid placement and suggest
    a very rudimentary folding path?? It would be really simple
    and probably mostly scientifically incorrect. This idea
    probably isn't going to get implemented.
    '''

---
+++ NA and protein converter.py
@@ -0,0 +1,17 @@
+def convert(startingType, endingType):
+    #GENERAL INFO
+    #------------------------------------------
+    #
+    #DNA Base Pairs:
+    #A-T, C-G
+    #Adenine and Thymine, Cytosine and Guanine
+    #
+    #RNA Base Pairs:
+    #A-U, C-G
+    #Adenine and Uracil, Cytosine and Guanine
+    #I know RNA is almost always single-stranded so bonding
+    #between the bases doesn't matter, but sometimes
+    #it happens!
+    #
+    #Amino Acids & Corresponding Codons:
+    #No idea. I'll look this up later.
