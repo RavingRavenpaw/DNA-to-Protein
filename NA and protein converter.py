88e49d03b4d3fc0033a3c17bae3c0fdcd7fc7623
Raven <chromefusion44@gmail.com>
2016-03-10 20:21:02 -0500

    Cleanup
    Don't need the baes pair info anymore, and I removed the convert
    function in its entirety as of now bc it's just taking up space. Maybe
    I'll just have the run through go from DNA to amino acids/a protein.
    Maybe I'll make the conversion ability modular. Idk yet.

--- NA and protein converter.py
+++ NA and protein converter.py
@@ -1,22 +1,3 @@
-def convert(startType, startSequence, endType, endSequence):
-    #GENERAL INFO
-    #------------------------------------------
-    #
-    #DNA Base Pairs:
-    #A-T, C-G
-    #Adenine and Thymine, Cytosine and Guanine
-    #
-    #RNA Base Pairs:
-    #A-U, C-G
-    #Adenine and Uracil, Cytosine and Guanine
-    #I know RNA is almost always single-stranded so bonding
-    #between the bases doesn't matter, but sometimes
-    #it happens!
-    #
-    #Amino Acids & Corresponding Codons:
-    #No idea. I'll look this up later.
-    print("test")
-
 def DNAtoRNA(DNAStrand1):
     #Initialization of variables
     timesDone = 0
